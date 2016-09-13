d8995a225a3a33455f3bfeaeaa84a0e2b3757c35
fdpeiter <fdpeiter@gmail.com>
2016-09-12 23:09:43 -0300

    Add files via upload

---
+++ client.c
@@ -0,0 +1,121 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h> 
+#include "queue.h"
+
+struct user_addr{
+    char* user_ip;
+    char* user_port;
+};
+
+int running = 1;
+Queue* Q;
+
+
+int communicate(struct sockaddr_in serv_addr,  int type){
+    int sockfd = 0;
+    /* Passo 1 - Criar socket */
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket: ");
+        exit(1);
+    }
+
+    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+        perror("connect: ");
+        exit(1);
+    }
+    int  n = 0;
+    int server_value =0 ;
+    if (send(sockfd,&type,1,0) <0)
+        printf ("error\n");
+    else
+        printf ("packet send done");
+    /* Enviar ou receber dados do servidor */
+    /* Enquanto o numero de bytes recebidos for differente de 0*/
+    if (recv(sockfd, &server_value, sizeof(int), 0) < sizeof(int))
+    {
+    	perror("receive error: ");
+    }
+    else{
+        /*Print value*/
+    }
+    return 1;
+}
+
+void * producer(int size){
+    int i;
+	for(i=0; i<size; i = i+2){
+            Enqueue(Q,0);
+            Enqueue(Q,1);
+	}
+}
+
+void * consumer(struct sockaddr_in serv_addr){
+	while(running){
+            int type = front(Q);
+            Dequeue(Q);
+            communicate(serv_addr, type);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int sockfd = 0, n = 0, numthr = 0;
+    pthread_t prod, *con;
+    char recvBuff[1024];
+    struct sockaddr_in serv_addr; 
+
+    if(argc != 5)
+    {
+        printf("\n Usage: %s <ip of server> <port> <num_workers> <num_threads>\n",argv[0]);
+        return 1;
+    }
+    if(atoi(argv[4]) % 2 != 0){
+        printf("\n Num of workers must be even!\n");
+        return 1;
+    }
+    Q = createQueue(atoi(argv[3]));
+    /* Passo 2 - Configura struct sockaddr_in */
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    /* converte Ip em formato string para o formato exigido pela struct sockaddr_in*/
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0){
+        printf("\n inet_pton error occured\n");
+        exit(1);
+    }
+    numthr = atoi(argv[4]);
+    con = (pthread_t *)malloc(numthr * sizeof(pthread_t));
+    if(pthread_create(&prod, NULL, producer, argv[3])) {
+        fprintf(stderr, "Error creating thread\n");
+	return 1;
+    }
+    for(i=0; i<numthr; i++){
+        if(pthread_create(&con[i], NULL, consumer, &serv_addr)) {
+	    fprintf(stderr, "Error creating thread\n");
+	    return 2;
+	}
+    if(pthread_join(prod, NULL)) {
+        fprintf(stderr, "Error joining thread\n");
+        return 3;
+    }
+    /* encerra execução das threads produtoras. */
+    running = 0;
+    for(i=0; i<numthr; i++){
+        if(pthread_join(con[i], NULL)) {
+            fprintf(stderr, "Error joining thread\n");
+            return 4;
+        }
+    }
+        
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,116 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+int total = 0;
+sem_t reader_sem, db_sem;
+int readCount = 0;
+
+void* handle_connection(void *arg)
+{
+    int client_sock = *(int*)arg;
+    int server_value = 0;
+    int client_type = 0;
+    if (client_type = recv(client_sock, &server_value, sizeof(int), 0) < sizeof(int)){
+        perror("receive: ");
+    }
+    else{
+        int result = 0;
+        if (client_type){
+            writer(&result);
+        }
+        else{
+            reader(&result);
+        }
+        if (send(sockfd,&result,1,0) <0)
+            perror("send: ");
+    }
+    
+}
+
+void* reader(int* result){
+    
+    readCount++;
+    int tempResult = 0;
+    
+    if (readCount == 1){
+        sem_wait(&db_sem);
+    }
+    
+    sem_post(&reader_sem);
+    tempResult = total;
+    sem_wait(&reader_sem);
+    
+    readCount--;
+    if(readCount==0){
+        sem_post(&db_sem);
+    }
+    sem_post(&reader_sem);
+    &result = total;
+    
+}
+void* writer(int* result){
+    sem_wait(&db_sem);
+    total++;
+    &result = total;
+    sem_post(&db_sem);
+}
+
+int main(int argc, char *argv[])
+{
+    sem_init(&readCountAccess,0,1);
+    sem_init(&databaseAccess,0,1);
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr, clientaddr;
+    char sendBuff[1025];
+    socklen_t sz;
+
+
+    memset(sendBuff, '0', sizeof(sendBuff)); 
+
+    /* Passo 1 - Criar Socket */
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+	  perror("socket: ");
+	  exit(1);
+    }
+
+    /* Passo 2 - Configurar estrutura sockaddr_in */
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5000); 
+    
+    /* Passo 3 - Associar socket com a estrutura sockaddr_in     */
+    if (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+   	  perror("bind: ");
+	  exit(1);
+    }
+
+    /* Passo 4 - Tornar o servidor ativo  */
+    listen(listenfd, 10); 
+
+    while(1)
+    {
+        /* Passo 5 - Aguardar conexão do cliente.  */
+        sz = sizeof(clientaddr);
+        if ((connfd = accept(listenfd, (struct sockaddr*)&clientaddr, &sz)) < 0){
+            perror("accept: ");
+            continue;
+        }
+        pthread_t client_threadid;
+        while((connfd = accept(listenfd, (struct sockaddr*)&clientaddr, &sz)) != -1)
+        {
+            pthread_create(&client_threadid,NULL,handle_connection, &connfd);
+        }
+        close(connfd);
+     }
+}
+
